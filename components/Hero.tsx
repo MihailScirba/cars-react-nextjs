42c4aef92aa17c9860efbed37038321785d6979a
MihailScirba <mihailscirba8@gmail.com>
2025-03-18 22:26:05 +0200

    Initial commit. Spend 1 hour to fix tailwind bugs due to different next.js version from video tutorial and my current version on pc

---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto'>Hero</div>
+  )
+}
+
+export default Hero
