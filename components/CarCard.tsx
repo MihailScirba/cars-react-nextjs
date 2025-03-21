e35e2113a295015f9ec813a2fcd08351c59673e8
MihailScirba <mihailscirba8@gmail.com>
2025-03-21 13:54:43 +0200

    Finsihed up car-card. I took not much time due to there was no bugs faced in comparison with previous parts. Add properties to custom button. Created card header with title, main image, mini info about car and button 'view more'

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -12,19 +12,79 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
   const carRent = calculateRent(city_mpg, year);
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <div className="car-card group">
+      {/* Header / title */}
       <div className="car-card__content">
         <h2 className="car-card__content-title">
           {make} {model}
         </h2>
-        <p className="car-card__price">
-          <span className="car-card__price-dollar">$</span>
-          {carRent}
-          <span className="car-card__price-day">/day</span>
-        </p>
       </div>
+
+      {/* Price */}
+      <p className="car-card__price">
+        <span className="car-card__price-dollar">$</span>
+        {carRent}
+        <span className="car-card__price-day">/day</span>
+      </p>
+
+      {/* Main image */}
+      <div className="car-card__image">
+        <Image
+          src="/hero.png"
+          alt="car image"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
+
+      {/* Bottom group */}
+      <div className="relative flex w-full mt-2">
+      
+        {/* Info */}
+        <div className="car-card__icon-container">
+          {/* Transmision icon */}
+          <div className="car-card__icon">
+            <Image
+              src="/steering-wheel.svg"
+              alt="wheel"
+              width={20}
+              height={20}
+            />
+            <p className="car-card__icon-text">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+
+          {/* Tire icon */}
+          <div className="car-card__icon">
+            <Image src="/tire.svg" alt="tire" width={20} height={20} />
+            <p className="car-card__icon-text">{drive.toUpperCase()}</p>
+          </div>
+
+          {/* Fuel icon */}
+          <div className="car-card__icon">
+            <Image src="/gas.svg" alt="mpg" width={20} height={20} />
+            <p className="car-card__icon-text">{city_mpg} MPG</p>
+          </div>
+        </div>
+        
+        {/* Button */}
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick = {() => setIsOpen(true)}
+          />
+        </div>
+      </div>
+      
+      {/* Car detail popup */}
     </div>
   );
 };
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -8,6 +8,9 @@ const CustomButton = ({
   title,
   btnType,
   containerStyles,
+  textStyles,
+  rightIcon,
+  isDisabled,
   handleClick,
 }: CustomButtonProps) => {
   return (
@@ -17,7 +20,14 @@ const CustomButton = ({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={"flex-1"}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {
+        rightIcon && (
+          <div className="relative w-6 h-6">
+            <Image src={rightIcon} alt="right arrow" fill object-contain/>
+          </div>
+        )
+      }
     </button>
   );
 };
