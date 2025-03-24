0292defdeed2ceb77300323c3372897c3a680f2d
MihailScirba <mihailscirba8@gmail.com>
2025-03-24 14:51:57 +0200

    Added second input in search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
 import SearchManufacturer from "./SearchManufacturer";
 
-
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="search"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
@@ -16,7 +27,27 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          alt="model"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+        />
+        <input
+          type="text"
+          name="model"
+          // value={""}
+          // onChange={(e) => setModel(e.target.value)}
+          placeholder="Passat"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 };
